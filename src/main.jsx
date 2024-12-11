8369a8bfe3041f605a3015f42f055494c1c4e250
KATKURI NIKHIL <knikhil07k@gmail.com>
2024-12-11 22:02:56 +0530

    edit

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { StrictMode } from "react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-<HashRouter>
+<BrowserRouter basename="/Portfilo/">
   <App />
-</HashRouter>
+</BrowserRouter>
       
     
   </StrictMode>
